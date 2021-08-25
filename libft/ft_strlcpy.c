c73d4a814e0ca5e08c5b164659fd31b4e796a619
xogml123 <xogml951@gmail.com>
2021-08-25 19:02:40 +0900

    final

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (size == 1 || src[0] == '\0')
+		dest[0] = '\0';
+	else if (size == 0)
+		j = 0;
+	else
+	{
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		if (i < size)
+			dest[i] = '\0';
+	}
+	while (src[j] != 0)
+		j++;
+	return (j);
+}
